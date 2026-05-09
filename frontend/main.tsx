fbfc3e12ab4a1c1f4a9ef23ed0de21d7f3c97675
Builder106 <vaughanolayinka@gmail.com>
2026-05-09 02:36:56 -0400

    fix: remove undefined Analytics component from main.tsx

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -13,7 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
          <AuthProvider>
             <ConfirmProvider>
                <App />
-               <Analytics />
             </ConfirmProvider>
             <Toaster
                position="bottom-right"
